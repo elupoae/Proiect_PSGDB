eee19d0273ebb38a39dcdbfb2de33bb8eb9b549c
Eduard Lupoae <edward_v98@yahoo.com>
2019-05-19 16:34:47 +0300

    Front terminat complet

---
+++ Train/javascript/chosen.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    // buy-ticket page
+    $("#choose-departure").chosen({no_results_text: "Oops, nothing found!"});
+    $("#choose-arrive").chosen({no_results_text: "Oops, nothing found!"});
+
+    // employees page
+    $("#choose-new-train").chosen({no_results_text: "Oops, nothing found!"});
+    $("#choose-job").chosen({no_results_text: "Oops, nothing found!"});
+    $("#choose-emp1").chosen({no_results_text: "Oops, nothing found!"});
+    $("#choose-emp2").chosen({no_results_text: "Oops, nothing found!"});
+    $("#choose-emp-train").chosen({no_results_text: "Oops, nothing found!"});
+
+    // trains page
+    $("#ch-dep-tr").chosen({no_results_text: "Oops, nothing found!"});
+    $("#ch-arr-tr").chosen({no_results_text: "Oops, nothing found!"});
+    $("#ch-carload").chosen({no_results_text: "Oops, nothing found!"});
+    $("#ch-status").chosen({no_results_text: "Oops, nothing found!"});
+
+    // stations & routes page
+    $("#ch-up-rt").chosen({no_results_text: "Oops, nothing found!"});
+    $("#ch-up-st").chosen({no_results_text: "Oops, nothing found!"});
+    $("#ch-arr-st").chosen({no_results_text: "Oops, nothing found!"});
+    $("#ch-dep-st").chosen({no_results_text: "Oops, nothing found!"});
+});
+
+
---
+++ Train/javascript/trains.js
@@ -0,0 +1,16 @@
+let classTables = document.getElementsByClassName('table');
+let classButton = document.getElementsByClassName('button-g-table');
+
+
+for (let i = 0; i <= classButton.length; i++) {
+    classButton[i].onclick = function() {
+        if(classTables[i].style.display === "none") {
+            classTables[i].style.display = 'block';
+            for (let j = 0; j <= classTables.length; j++) {
+                if( i !== j){
+                    classTables[j].style.display = 'none';
+                }
+            }
+        }
+    };
+}
